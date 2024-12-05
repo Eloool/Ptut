b40ab1915ff795a9ad3dd5b2516cb694c5196413
Lordsamourai <157367778+Lordsamourai@users.noreply.github.com>
2024-12-05 16:40:34 +0100

    Update

---
+++ Ptut/Assets/Scripts/RecipeData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Recipe", menuName = "Recipes/New Recipe")]
+public class RecipeData : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
