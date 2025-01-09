c6ec9cba7a3ef258d5ad5867287f58fbc6884674
enzB29 <enzo.bordet43@gmail.com>
2025-01-09 09:17:18 +0100

    test

--- Ptut/Assets/Scripts/SpawnerAnimals.cs
+++ Ptut/Assets/Scripts/SpawnerAnimals.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class SpawnerBear : MonoBehaviour
+public class SpawnerAnimals : MonoBehaviour
 {
     public GameObject Bear;
+    public GameObject Goat;
+
+
     public int MaxHighOfMap = 0;
     private int MinXMap;
     private int MinZMap;
@@ -19,7 +22,13 @@ public class SpawnerBear : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        TailleChunk = TailleMap/nbChunkInLine;
+        BearSpawner();
+        GoatSpawner();
+    }
+
+    void BearSpawner()
+    {
+        TailleChunk = TailleMap / nbChunkInLine;
         for (int i = 0; i < nbChunkInLine; i++)
         {
             MinXMap = i * TailleChunk;
@@ -28,7 +37,7 @@ public class SpawnerBear : MonoBehaviour
             {
                 float rand = Random.value;
                 //Debug.Log("valeur aléat générée : " +  rand + " pour le chunk : " + i + ", "+j);
-                if (rand < 9f/64f)//en moyenne 9 ours seront générés parmis les 64 chunks (8 par ligne et 8 par colonne)
+                if (rand < 9f / 64f)//en moyenne 9 ours seront générés parmis les 64 chunks (8 par ligne et 8 par colonne)
                 {
                     MinZMap = j * TailleChunk;
                     MaxZMap = j * TailleChunk + TailleChunk;
@@ -42,5 +51,27 @@ public class SpawnerBear : MonoBehaviour
         }
     }
 
-    
+    void GoatSpawner()
+    {
+        TailleChunk = TailleMap / nbChunkInLine;
+        for (int i = 0; i < nbChunkInLine; i++)
+        {
+            MinXMap = i * TailleChunk;
+            MaxXMap = i * TailleChunk + TailleChunk;
+            for (int j = 0; j < nbChunkInLine; j++)
+            {
+                float rand = Random.value;
+                if (rand < 10f / 64f)//en moyenne 10 chevre seront générés parmis les 64 chunks (8 par ligne et 8 par colonne)
+                {
+                    MinZMap = j * TailleChunk;
+                    MaxZMap = j * TailleChunk + TailleChunk;
+                    int spawnX = Random.Range(MinXMap, MaxXMap);
+                    int spawnZ = Random.Range(MinZMap, MaxZMap);
+                    Vector3 spawnPos = new Vector3(spawnX, MaxHighOfMap, spawnZ);
+                    Instantiate(Goat, spawnPos, Quaternion.identity);
+                }
+            }
+        }
+    }
+
 }
